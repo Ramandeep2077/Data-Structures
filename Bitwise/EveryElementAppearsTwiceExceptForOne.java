73ad733c560d7d614310c50a40cd6d8c1dc1ba6c
Raman <Ramandeep2077@gmail.com>
2023-08-24 00:35:05 +0530

    Find the only non repeating element in an array where every element repeats twice

--- Bitwise/EveryElementAppearsTwiceExceptForOne.java
+++ Bitwise/EveryElementAppearsTwiceExceptForOne.java
@@ -1,6 +1,7 @@
 class Solution {
-    public int singleNumber(int[] nums) {
 
+    public int singleNumber(int[] nums) {
+//   nums= { 2,2,1 };
        int uniq=0;
        for(int i: nums){
            uniq= uniq^i;
