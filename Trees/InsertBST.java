5552b3fff0848f9371fc91d8b37dd603dcc1f7e4
Raman <Ramandeep2077@gmail.com>
2023-10-17 02:41:09 +0530

    Insert into a Binary Search Tree

--- Trees/InsertBST.java
+++ Trees/InsertBST.java
@@ -1,4 +1,11 @@
-class InsertBST{
+/*
+The worst-case time complexity of insert operations is O(h) where h is the height of the Binary Search Tree. 
+In the worst case, we may have to travel from the root to the deepest leaf node. The height of a skewed tree may become n and the time complexity of insertion operation may become O(n). 
+
+Auxiliary Space: The auxiliary space complexity of insertion into a binary search tree is O(1)
+
+*/
+class InsertBST {
 	public TreeNode insertIntoBST(TreeNode root, int val) {
 
 		if (root == null) {
