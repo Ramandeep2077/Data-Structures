ee464f2457c5fdc2a6c7de250c12034bb77906b9
Raman <Ramandeep2077@gmail.com>
2023-10-18 01:58:03 +0530

    Balance a Binary Search Tree

---
+++ Trees/BalanceBST.java
@@ -0,0 +1,35 @@
+class Solution {
+	List<TreeNode> list = new ArrayList<>();
+
+	public TreeNode balanceBST(TreeNode root) {
+		inOrder(root);
+
+		return construct(0, list.size() - 1);
+
+	}
+
+	public TreeNode construct(int start, int end) {
+
+		if (start > end)
+			return null;
+
+		int mid = start + (end - start) / 2;
+		TreeNode root = list.get(mid);
+
+		root.left = construct(start, mid - 1);
+		root.right = construct(mid + 1, end);
+
+		return root;
+
+	}
+
+	public void inOrder(TreeNode root) {
+		if (root == null)
+			return;
+
+		inOrder(root.left);
+		list.add(root);
+		inOrder(root.right);
+
+	}
+}
