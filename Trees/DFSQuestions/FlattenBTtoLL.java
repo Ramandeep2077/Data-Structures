c22b6dbf4e01308801ba778225f6b31c35a7f917
Ramandeep2077 <Ramandeep2077@gmail.com>
2023-11-20 19:32:03 +0530

    Flatten Binary Tree to Linked List

---
+++ Trees/DFSQuestions/FlattenBTtoLL.java
@@ -0,0 +1,20 @@
+class FlattenBTtoLL {
+
+	public void flatten(TreeNode root) {
+		TreeNode current = root;
+		while (current != null) {
+			if (current.left != null) {
+				TreeNode temp = current.left;
+				while (temp.right != null) {
+					temp = temp.right;
+				}
+
+				temp.right = current.right;
+				current.right = current.left;
+				current.left = null;
+			}
+			current = current.right;
+		}
+
+	}
+}
